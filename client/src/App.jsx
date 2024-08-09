d4e426b2a8a5c7a98a2e7eb234914a4a8704294f
Senin Adın <senin_email_adresin@example.com>
2024-08-09 06:24:57 -0800

    added the header component and updated the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import About from "./pages/about";
+import Profile from "./pages/profile";
+import Header from "./assets/components/header";
 
 function App() {
   return (
-    <div className='text-red-500'>
-      <p>hlelo</p>
-    </div>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Root route */}
+        <Route path="/sign-in" element={<Signin />} /> 
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Singup = () => {
+  return (
+    <div>
+      signup
+    </div>
+  )
+}
+
+export default Singup
