58797e367931e193569fcce61aa4f72ab835e4c4
Senin Adın <senin_email_adresin@example.com>
2024-08-10 07:28:51 -0800

    database connected

---
+++ api/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then( () => {
+  console.log("connected to mongodb");
+}).catch((err)=> {
+  console.log("eror"+ err );
+})
+
+ 
+const app = express();
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
